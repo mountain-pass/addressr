8acb6dda396b2f3a437bad87f536ff85086ac882
Tom Howard <tom@windyroad.com.au>
2020-02-12 22:31:55 +1100

    removed gclou stackdriver exports

--- service/Monitoring.js
+++ service/Monitoring.js
@@ -4,60 +4,16 @@ import {
   MeasureUnit,
   TagMap,
 } from '@opencensus/core';
-import {
-  StackdriverStatsExporter,
-  StackdriverTraceExporter,
-} from '@opencensus/exporter-stackdriver';
+// import {
+//   StackdriverStatsExporter,
+//   StackdriverTraceExporter,
+// } from '@opencensus/exporter-stackdriver';
 // var os = require('os');
 import tracing from '@opencensus/nodejs';
 import dotenv from 'dotenv';
 import { machineId } from 'node-machine-id';
 
 dotenv.config();
-
-const GCLOUD_PROJECT = process.env.GCLOUD_PROJECT || 'addressr-249104';
-
-const GOOGLE_CREDENTIALS = {
-  type: process.env.GOOGLE_CREDENTIALS_TYPE || 'service_account',
-  project_id: GCLOUD_PROJECT,
-  private_key_id:
-    process.env.GOOGLE_CREDENTIALS_private_key_id ||
-    '3a621535318cb91b39aef8f94d9c99d4867517b5',
-  private_key:
-    process.env.GOOGLE_CREDENTIALS_private_key ||
-    '-----BEGIN PRIVATE KEY-----\nXXXXX\n-----END PRIVATE KEY-----\n',
-  client_email:
-    process.env.GOOGLE_CREDENTIALS_client_email ||
-    'xxx@xxxx.iam.gserviceaccount.com',
-  client_id:
-    process.env.GOOGLE_CREDENTIALS_client_id || '112640684218263046489',
-  auth_uri:
-    process.env.GOOGLE_CREDENTIALS_auth_uri ||
-    'https://accounts.google.com/o/oauth2/auth',
-  token_uri:
-    process.env.GOOGLE_CREDENTIALS_token_uri ||
-    'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url:
-    process.env.GOOGLE_CREDENTIALS_auth_provider_x509_cert_url ||
-    'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:
-    process.env.GOOGLE_CREDENTIALS_client_x509_cert_url ||
-    'https://www.googleapis.com/robot/v1/metadata/x509/xxxxx%40xxxxx.iam.gserviceaccount.com',
-};
-
-// Add your project id to the Stackdriver options
-const traceExporter = new StackdriverTraceExporter({
-  projectId: GCLOUD_PROJECT,
-  credentials: GOOGLE_CREDENTIALS,
-});
-
-const statsExporter = new StackdriverStatsExporter({
-  projectId: GCLOUD_PROJECT,
-  credentials: GOOGLE_CREDENTIALS,
-});
-
-// Pass the created exporter to Stats
-globalStats.registerExporter(statsExporter);
 
 export function getTracer() {
   return tracing.tracer;
@@ -66,16 +22,6 @@ export function getTracer() {
 export async function initMonitoring(component) {
   const mId = await machineId();
   const pId = process.pid;
-
-  tracing.registerExporter(traceExporter).start({
-    samplingRate: 1,
-    defaultAttributes: {
-      environment: process.env.NODE_ENV || 'development',
-      job: component,
-      task_id: `${mId}-${pId}`,
-    },
-    stats: globalStats,
-  });
 
   let previousUsage = process.cpuUsage();
   let sampleDate = Date.now();
@@ -163,5 +109,5 @@ export async function initMonitoring(component) {
   );
 
   const monitoringInterval = 0;
-  return { tracing: tracing, monitoringInterval, traceExporter };
+  return { tracing: tracing, monitoringInterval };
 }
